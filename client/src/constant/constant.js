7f494cd0e652aafbef494ca8b1e147f279d454fe
Shubham Kumar <66840922+Shubham5213@users.noreply.github.com>
2024-01-24 02:25:15 +0530

    Added hoisted Server url to client

--- client/src/constant/constant.js
+++ client/src/constant/constant.js
@@ -1,2 +1,2 @@
-export const apiUrl = "http://localhost:8000";
-// export const apiUrl = "https://sixthsens-server.onrender.com";
+// export const apiUrl = "http://localhost:8000";
+export const apiUrl = "https://sixthsens-server.onrender.com";
